2edcf981c465ae3e11a6d5c25ba4ff4ee5baf806
Angela Silva <aggsdpz@gmail.com>
2023-06-25 21:20:30 -0300

    inicio do form de cadastro

--- fullstack-cardapio/cardapio/cardapio-front/cardapio/src/App.tsx
+++ fullstack-cardapio/cardapio/cardapio-front/cardapio/src/App.tsx
@@ -3,16 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Card } from './components/card/card';
-import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
 
-  const data: FoodData[] = [];
+  const {data} = useFoodData();
   return (
     <div className="container">
       <h1>Cardapio</h1>
       <div className="card-grid">
-        {data.map(foodData => 
+        {data?.map(foodData => 
           <Card 
             price={foodData.price} 
             title={foodData.title} 
---
+++ fullstack-cardapio/cardapio/cardapio-front/cardapio/src/components/card/create-modal/create-modal.tsx
@@ -0,0 +1,16 @@
+interface CreateModalProps {
+
+}
+
+export function CreateModal(props : CreateModalProps){
+    return (
+        <div className="modal-overflow">
+            <div className="modal-body">
+                <h2>Cadastre um novo item no cardápio</h2>
+                <form action="" className="input-container">
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
