04192323f20724a634e4ba0a7b7c9709e15d72bc
Angela Silva <aggsdpz@gmail.com>
2023-07-23 23:16:41 -0300

    homepage

--- book-tracker/front/assets/js/homepage.js
+++ book-tracker/front/assets/js/homepage.js
@@ -1,5 +1,9 @@
 const itemInput = document.getElementById("itemInput");
 const itemList = document.getElementById("itemList");
+const images = ["slides1.jpg", "slides2.jpg", "slides3.jpg", "slides4.jpg"];
+let currentSlide = 0;
+const intervalTime = 5000; 
+
 
 itemInput.addEventListener("keyup", function(event) {
   // Verifica se a tecla pressionada é a tecla "Enter" (código 13)
@@ -22,3 +26,21 @@ function addItemToList() {
 
   itemInput.value = ""; // Limpa o campo de entrada após adicionar o item
 }
+
+function changeBackground(imageName) {
+    const home = document.querySelector(".home");
+    home.style.transition = "background-image 0.5s ease-in-out";
+    home.style.backgroundImage = `url(assets/images/${imageName})`;
+    home.style.backgroundPosition = "center center";
+    home.style.opacity = 0.8; 
+}
+
+function startSlideShow() {
+    setInterval(() => {
+        currentSlide = (currentSlide + 1) % images.length;
+        changeBackground(images[currentSlide]);
+    }, intervalTime);
+}
+
+changeBackground(images[0]);
+startSlideShow();
